8141e1a582426de5236cfe10964b9de165b1d28d
ulldecorb <ausias.bertran@gmail.com>
2022-09-20 14:56:58 +0200

    Uninstall react Create react app Install eslint dependency Delete unused modules

---
+++ index.js
@@ -0,0 +1,14 @@
+/* - Dadas estas dos APIs:
+    - https://catfact.ninja/fact
+    - https://developers.giphy.com/docs/
+   Selecciona um dato sobre gatos usando la primera API.
+   De ese dato, usa las tres primeras palabras 
+   y busca un gif usando la API Key de Giphy.
+
+   El resultado se tiene que mostrar con una imagen a la izquierda 
+   y el texto a la derecha, todo centrado verticalmente.
+
+   La API KEY de Gliphy:
+   const GIPHY_API_KEY = "2cZkiFTqyiS79UdSapL6LHWlublpl7iY"
+*/
+
