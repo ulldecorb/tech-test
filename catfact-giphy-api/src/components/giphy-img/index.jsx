55ccf49495e6c9e5da95576028f6b66e24da93d2
ulldecorb <ausias.bertran@gmail.com>
2022-09-27 00:30:26 +0200

    Reduce url response to src img element

--- catfact-giphy-api/src/components/giphy-img/index.jsx
+++ catfact-giphy-api/src/components/giphy-img/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { getImgUrl } from '../../services';
 
 function GiphyImg({ imgParams }) {
+  const [src, setSrc] = useState('');
+
+  useEffect(() => {
+    getImgUrl(imgParams, setSrc);
+  });
+
   return (
-    <div>{imgParams}</div>
+    <img src={src} alt="gif" />
   );
 }
 
 GiphyImg.propTypes = {
-  imgParams: PropTypes.func.isRequired
+  imgParams: PropTypes.string.isRequired
 };
 
 export default GiphyImg;
--- catfact-giphy-api/src/services/index.js
+++ catfact-giphy-api/src/services/index.js
@@ -1,4 +1,7 @@
 const catApi = 'https://catfact.ninja/fact';
+const giphyUrl = 'https://api.giphy.com/v1/gifs/search';
+const GiphyApiKey = 'hrvVWybnAzoz6NV9L1tM72aMPVgY1P3r';
+
 // const giphyApi = 'https://developers.giphy.com/docs/';
 
 // export async function fetchCat(setState) {
@@ -10,8 +13,15 @@ const catApi = 'https://catfact.ninja/fact';
 //   return catText;
 // }
 
-export function convertStringToSearchParam(str) {
+function convertStringToSearchParam(str) {
   return str.split(' ').map((letter) => letter.toLowerCase()).slice(0, 3).join('&20');
+}
+
+function convertUrltoUri(str) {
+  const regexp = /media./;
+  const cutIndex = str.indexOf('?');
+  const newUrl = str.slice(0, cutIndex).replace(regexp, 'i');
+  return newUrl;
 }
 
 export async function fetchCat(setCatTextState, setSearchParam) {
@@ -21,5 +31,18 @@ export async function fetchCat(setCatTextState, setSearchParam) {
       setCatTextState(data.fact);
       const newSearchParam = convertStringToSearchParam(data.fact);
       setSearchParam(newSearchParam);
+    });
+}
+
+export async function getImgUrl(searchParams, setState) {
+  const newUrl = `${giphyUrl}?&api_key=${GiphyApiKey}&q=${searchParams}`;
+  await fetch(newUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('data ', data);
+      const defaultUrl = data.data[0].images.original.webp;
+
+      const newSrc = convertUrltoUri(defaultUrl);
+      setState(newSrc);
     });
 }
