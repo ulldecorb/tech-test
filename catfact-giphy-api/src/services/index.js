f708da32e889128844979ca5b92c132e109c9092
ulldecorb <ausias.bertran@gmail.com>
2022-09-27 01:00:52 +0200

    Add sass style

--- catfact-giphy-api/src/services/index.js
+++ catfact-giphy-api/src/services/index.js
@@ -2,17 +2,6 @@ const catApi = 'https://catfact.ninja/fact';
 const giphyUrl = 'https://api.giphy.com/v1/gifs/search';
 const GiphyApiKey = 'hrvVWybnAzoz6NV9L1tM72aMPVgY1P3r';
 
-// const giphyApi = 'https://developers.giphy.com/docs/';
-
-// export async function fetchCat(setState) {
-//   const catText = await fetch(catApi)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       setState(data.fact);
-//     });
-//   return catText;
-// }
-
 function convertStringToSearchParam(str) {
   return str.split(' ').map((letter) => letter.toLowerCase()).slice(0, 3).join('&20');
 }
